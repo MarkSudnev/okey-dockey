eb87a6053f5660b3797b23bf809765ee1ee0e461
Markel Sudnev <m.sudnev@gmail.com>
2025-07-23 23:48:04 +0200

    purple-app: update message handler

--- purple-app/app/presentation/src/main/kotlin/pl/sudneu/purple/presentation/PurpleMessageHandler.kt
+++ purple-app/app/presentation/src/main/kotlin/pl/sudneu/purple/presentation/PurpleMessageHandler.kt
@@ -1,5 +1,6 @@
 package pl.sudneu.purple.presentation
 
+import dev.forkhandles.result4k.peek
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.common.errors.WakeupException
 import pl.sudneu.purple.domain.ReceiveDocumentMetadata
@@ -13,16 +14,26 @@ class PurpleMessageHandler(
 
   fun listen(topicName: String) {
     consumer.subscribe(setOf(topicName))
+    Runtime.getRuntime().addShutdownHook(Thread { stop() })
     try {
       consume(Duration.ofMillis(100))
     }
     catch (_: WakeupException) {}
+    finally {
+        consumer.close()
+    }
   }
 
   private fun consume(duration: Duration) {
     while(true) {
       val records = consumer.poll(duration)
-      records.forEach { receiveDocumentMetadata(it.value().toDocumentMetadata()) }
+      records.forEach {
+        receiveDocumentMetadata(it.value().toDocumentMetadata()).peek { consumer.commitSync() }
+      }
     }
+  }
+
+  private fun stop() {
+    consumer.wakeup()
   }
 }
--- purple-app/app/presentation/src/test/kotlin/pl/sudneu/purple/presentation/PurpleMessageHandlerShould.kt
+++ purple-app/app/presentation/src/test/kotlin/pl/sudneu/purple/presentation/PurpleMessageHandlerShould.kt
@@ -2,13 +2,19 @@ package pl.sudneu.purple.presentation
 
 import dev.forkhandles.fabrikate.Fabrikate
 import dev.forkhandles.result4k.Success
+import dev.forkhandles.result4k.asFailure
 import dev.forkhandles.result4k.asSuccess
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.maps.shouldBeEmpty
+import io.kotest.matchers.maps.shouldHaveKey
+import io.kotest.matchers.maps.shouldHaveSize
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.MockConsumer
+import org.apache.kafka.clients.consumer.OffsetAndMetadata
 import org.apache.kafka.clients.consumer.OffsetResetStrategy
 import org.apache.kafka.common.TopicPartition
 import org.junit.jupiter.api.BeforeEach
@@ -19,6 +25,7 @@ import pl.sudneu.purple.domain.EmbedDocument
 import pl.sudneu.purple.domain.EmbeddedDocument
 import pl.sudneu.purple.domain.FetchDocument
 import pl.sudneu.purple.domain.NonBlankString
+import pl.sudneu.purple.domain.PurpleError
 import pl.sudneu.purple.domain.StoreDocument
 import java.util.*
 
@@ -31,6 +38,7 @@ class PurpleMessageHandlerShould {
   private val topicPartition = TopicPartition(topicName, 0)
   private val consumer =
     MockConsumer<String, FileReceivedEvent>(OffsetResetStrategy.EARLIEST)
+  private val randomEvent: FileReceivedEvent = Fabrikate().random()
 
   @BeforeEach
   fun setup() {
@@ -45,46 +53,62 @@ class PurpleMessageHandlerShould {
 
   @Test
   fun `subscribe to topic`() {
-    val consumer = MockConsumer<String, FileReceivedEvent>(OffsetResetStrategy.EARLIEST)
     val handler = PurpleMessageHandler(consumer, documentMetadataReceiver())
-
+    prepareConsumer()
+    consumer.schedulePollTask { sendMessage() }
+    stopConsumer()
     handler.listen(topicName)
-
     consumer.subscription() shouldContain topicName
   }
 
   @Test
   fun `call document metadata handler`() {
     val storage = mutableListOf<EmbeddedDocument>()
-    val event: FileReceivedEvent = Fabrikate().random()
-    prepareConsumer()
     val documentMetadataReceiver =
       documentMetadataReceiver(storeDocument = DummyDocumentStorer(storage))
     val handler = PurpleMessageHandler(consumer, documentMetadataReceiver)
-    consumer.schedulePollTask {
-      consumer.addRecord(
-        ConsumerRecord(
-          topicPartition.topic(),
-          topicPartition.partition(),
-          0,
-          "key",
-          event
-        )
-      )
-    }
-    consumer.schedulePollTask { consumer.wakeup() }
+    prepareConsumer()
+    consumer.schedulePollTask { sendMessage() }
+    stopConsumer()
     handler.listen(topicPartition.topic())
 
     storage.shouldNotBeEmpty()
   }
 
   @Test
-  fun `handle message`() {
-    val consumer = MockConsumer<String, FileReceivedEvent>(OffsetResetStrategy.EARLIEST)
-    val documentMetadataReceiver = documentMetadataReceiver()
-    val handler = PurpleMessageHandler(consumer, documentMetadataReceiver)
+  fun `commit handled message`() {
+    val handler = PurpleMessageHandler(consumer, documentMetadataReceiver())
+    prepareConsumer()
+    consumer.schedulePollTask { sendMessage() }
+    lateinit var committed: MutableMap<TopicPartition, OffsetAndMetadata>
+    consumer.schedulePollTask { committed = consumer.committed(setOf(topicPartition)) }
+    stopConsumer()
+    handler.listen(topicPartition.topic())
+    committed shouldHaveSize 1
+    committed shouldHaveKey topicPartition
+  }
 
-    handler.listen(topicName)
+  @Test
+  fun `not commit when document metadata handler is failed`() {
+    val handler =
+      PurpleMessageHandler(consumer) { PurpleError.UnknownError.asFailure() }
+    prepareConsumer()
+    consumer.schedulePollTask { sendMessage() }
+    lateinit var committed: MutableMap<TopicPartition, OffsetAndMetadata>
+    consumer.schedulePollTask { committed = consumer.committed(setOf(topicPartition)) }
+    stopConsumer()
+    handler.listen(topicPartition.topic())
+    committed.shouldBeEmpty()
+  }
+
+  @Test
+  fun `close consumer after shutdown`() {
+    val handler = PurpleMessageHandler(consumer, documentMetadataReceiver())
+    prepareConsumer()
+    consumer.schedulePollTask { sendMessage() }
+    stopConsumer()
+    handler.listen(topicPartition.topic())
+    consumer.closed() shouldBe true
   }
 
   private fun documentMetadataReceiver(
@@ -95,13 +119,25 @@ class PurpleMessageHandlerShould {
     fetchDocument, embedDocument, storeDocument
   )
 
+  private fun sendMessage(event: FileReceivedEvent = randomEvent) {
+    consumer.addRecord(
+      ConsumerRecord(
+        topicPartition.topic(),
+        topicPartition.partition(),
+        0,
+        "key",
+        event
+      )
+    )
+  }
+
   private fun prepareConsumer() {
     val offset = 0L
     consumer.updateBeginningOffsets(mapOf(topicPartition to offset))
     consumer.schedulePollTask { consumer.rebalance(mutableListOf(topicPartition)) }
   }
 
-  private fun stop() {
+  private fun stopConsumer() {
     consumer.schedulePollTask { consumer.wakeup() }
   }
 }
