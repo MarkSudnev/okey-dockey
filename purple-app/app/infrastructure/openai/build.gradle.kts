2be181ab35688d6cab8fff01ed5f302b6a687fd1
Markel Sudnev <m.sudnev@gmail.com>
2025-07-26 00:41:06 +0200

    purple-app: update open ai embedder

--- purple-app/app/infrastructure/openai/build.gradle.kts
+++ purple-app/app/infrastructure/openai/build.gradle.kts
@@ -4,4 +4,7 @@ plugins {
 
 dependencies {
   implementation(project(":app:domain"))
+  implementation(platform(libs.http4k.bom))
+  implementation(libs.http4k.core)
+  implementation(libs.jackson.kotlin)
 }
--- purple-app/app/infrastructure/openai/src/test/kotlin/pl/sudneu/purple/infrastructure/openai/OpenAiEmbedDocumentShould.kt
+++ purple-app/app/infrastructure/openai/src/test/kotlin/pl/sudneu/purple/infrastructure/openai/OpenAiEmbedDocumentShould.kt
@@ -1,20 +1,31 @@
 package pl.sudneu.purple.infrastructure.openai
 
 import dev.forkhandles.result4k.asSuccess
+import dev.forkhandles.result4k.kotest.shouldBeSuccess
+import org.http4k.core.HttpHandler
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Test
+import pl.sudneu.purple.domain.Document
 import pl.sudneu.purple.domain.EmbedDocument
 import pl.sudneu.purple.domain.EmbeddedDocument
-import java.util.Vector
+import java.util.*
 
 class OpenAiEmbedDocumentShould {
 
   @Test
   fun `get embeddings from open ai`() {
-
+    val httpClient: HttpHandler = { Response(OK).body(openAiResponseBody()) }
+    val document = Document("Hac vel parturient consectetur diam porta.")
+    val embedDocument = OpenAiEmbedDocument()
+    embedDocument(document) shouldBeSuccess EmbeddedDocument(document.content, Vector<Double>(10))
   }
 }
 
+fun openAiResponseBody(): String =
+  ClassLoader.getSystemResource("open-ai-embeddings-response.json").readText()
+
 fun OpenAiEmbedDocument(): EmbedDocument =
-  EmbedDocument {
-    EmbeddedDocument("Hello", Vector<Double>(10)).asSuccess()
+  EmbedDocument { document: Document ->
+    EmbeddedDocument(document.content, Vector<Double>(10)).asSuccess()
   }
