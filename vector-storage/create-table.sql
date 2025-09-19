908bb0f2f3e881cf836a9387333631c32101ca70
Markel Sudnev <m.sudnev@gmail.com>
2025-09-19 20:42:27 +0200

    change model for embedding

--- vector-storage/create-table.sql
+++ vector-storage/create-table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS documents (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     filename VARCHAR NOT NULL,
     content VARCHAR NOT NULL,
-    embedding vector(1152) NOT NULL
+    embedding vector(3072) NOT NULL
 );
